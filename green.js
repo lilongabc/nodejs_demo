317e609c1e818be7182449b643fb786e3cd069dc
lilongabc <lilongf5@gmail.com>
2018-07-16 11:22:21 +0800

    Add files via upload

---
+++ green.js
@@ -0,0 +1,16 @@
+﻿
+var http = require("http");
+var fs = require("fs");
+
+
+var server = http.createServer(function(req,res){
+	if(req.url == "/"){
+		fs.readFile("./green_roundness.html",function(err,data){
+
+			res.writeHead(200,{"Content-type":"text/html;charset=UTF-8"});
+			res.end(data);
+		});
+	}
+});
+
+server.listen(4000,"192.168.0.172");
